7c324ba52f93ffd986193bb71e9a77e24a0d83fc
Marcelino Sileoni <mnsileoni@gmail.com>
2022-11-28 16:45:03 -0300

    05_State_Events

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types';
 import './App.css';
 import pokemon from './pokemon.json';
 
-const PokemonRow = ({cadapokemon}) => (
-            <tr>
-              <td>{cadapokemon.name.english}</td>
-              <td>{cadapokemon.type.join(', ')}</td>
-          </tr>
+const PokemonRow = ({cadapokemon, siSelecciono}) => (
+  <tr>
+    <td>{cadapokemon.name.english}</td>
+    <td>{cadapokemon.type.join(', ')}</td>
+    <td>
+      <button onClick={() => siSelecciono(cadapokemon)}>Seleccionar</button>
+    </td>
+  </tr>
 )
 // Con propTypes verificamos que los tipos de datos esperados se cumplan.
 // Si no se cumplen aparece el error en consola del browser
@@ -18,9 +21,42 @@ PokemonRow.propTypes = {
     }).isRequired,
     type: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
+  siSelecciono: PropTypes.func,
+};
+
+const PokemonInfo = ({ name, base }) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      {Object.keys(base).map((clave) => (
+        <tr key={clave}>
+          <td>{clave}</td>
+          <td>{base[clave]}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+  cadapokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }).isRequired,
+    base: PropTypes.shape({
+      HP: PropTypes.number.isRequired,
+      Attack: PropTypes.number.isRequired,
+      Defense: PropTypes.number.isRequired,
+      "Sp. Attack": PropTypes.number.isRequired,
+      "Sp. Defense": PropTypes.number.isRequired,
+      Speed: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [selectedItem, selectedItemSet] = React.useState("");
 
   const divStyle = { // el estilo 
     margin: "auto",
@@ -31,7 +67,18 @@ function App() {
   return (
     <div  style={divStyle}>
       <h1 className="title">Pokemon Search</h1>
-      <table width="100%">
+
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "70% 30%",
+        gridColumnGap: '1rem',
+      }}>
+        <div>
+      <input
+        type="text"
+        value={filter}
+            onChange={(evento) => filterSet(evento.target.value)} />
+                  <table width="100%">
         <thead>
           <tr>
             <th>Nombre</th>
@@ -39,11 +86,15 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {pokemon.slice(0, 25).map(cadapokemon => (
-            <PokemonRow key={cadapokemon.id} cadapokemon={cadapokemon}/>
+          {pokemon.filter((unpokemon)=> unpokemon.name.english.toLowerCase().includes(filter.toLowerCase())).slice(0, 25).map(cadapokemon => (
+            <PokemonRow key={cadapokemon.id} cadapokemon={cadapokemon} siSelecciono={(cadapokemon) => selectedItemSet(cadapokemon) } />
           ))}
         </tbody>
       </table>
+
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem}/>}
+      </div>
     </div>
   );
 }
