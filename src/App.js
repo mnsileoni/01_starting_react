5b3c9930885ece27b44855be546f06a4d6ae1008
Marcelino Sileoni <mnsileoni@gmail.com>
2022-11-28 11:38:02 -0300

    Introduciong JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const divStyle = { // el estilo 
+    margin: "auto",
+    width: 800,
+    paddingTop: "1rem",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={divStyle}>
+      <h1 className="title">Pokemon Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>NombrePokemon</td>
+            <td>Poderes, muchos</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
